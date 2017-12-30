ad4e897847c2dc4faa224ab7673547780a11c650
tattn (Tatsuya Tanaka) <tanakasan2525@gmail.com>
2017-12-31 01:22:17 +0900

    Add TestablePanGestureRecognizer

---
+++ Tests/TestablePanGestureRecognizer.swift
@@ -0,0 +1,54 @@
+//
+//  TestablePanGestureRecognizer.swift
+//  SwipeTransitionTests
+//
+//  Created by Tatsuya Tanaka on 20171230.
+//  Copyright © 2017年 tattn. All rights reserved.
+//
+
+import UIKit
+
+class TestablePanGestureRecognizer: UIPanGestureRecognizer {
+    let testTarget: AnyObject?
+    let testAction: Selector?
+
+    var testState: UIGestureRecognizerState?
+    var testLocation: CGPoint?
+    var testTranslation: CGPoint?
+
+    override init(target: Any?, action: Selector?) {
+        testTarget = target as AnyObject
+        testAction = action
+        super.init(target: target, action: action)
+    }
+
+    func perfomTouch(location: CGPoint?, translation: CGPoint?, state: UIGestureRecognizerState) {
+        testLocation = location
+        testTranslation = translation
+        testState = state
+        if let testAction = testAction {
+            (testTarget as AnyObject).perform(testAction, on: .current, with: self, waitUntilDone: true)
+        }
+    }
+
+    override func location(in view: UIView?) -> CGPoint {
+        if let testLocation = testLocation {
+            return testLocation
+        }
+        return super.location(in: view)
+    }
+
+    override func translation(in view: UIView?) -> CGPoint {
+        if let testTranslation = testTranslation {
+            return testTranslation
+        }
+        return super.translation(in: view)
+    }
+
+    override var state: UIGestureRecognizerState {
+        if let testState = testState {
+            return testState
+        }
+        return super.state
+    }
+}
