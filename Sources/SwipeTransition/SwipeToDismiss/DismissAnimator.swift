bd9b61b6449806f0f372df518621dc35e6e35723
tattn (Tatsuya Tanaka) <tanakasan2525@gmail.com>
2018-01-24 00:15:47 +0900

    Add DismissAnimator

---
+++ Sources/SwipeTransition/SwipeToDismiss/DismissAnimator.swift
@@ -0,0 +1,45 @@
+//
+//  DismissAnimator.swift
+//  SwipeTransition
+//
+//  Created by Tatsuya Tanaka on 20180119.
+//  Copyright © 2018年 tattn. All rights reserved.
+//
+
+import UIKit
+
+final class DismissAnimator: NSObject {
+    weak var parent: SwipeToDismissController!
+    convenience init(parent: SwipeToDismissController) {
+        self.init()
+        self.parent = parent
+    }
+}
+
+extension DismissAnimator: UIViewControllerAnimatedTransitioning {
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return SwipeBackConfiguration.shared.transitionDuration
+    }
+
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        guard let from = transitionContext.viewController(forKey: .from) else { return }
+
+        parent.onStartTransition?(transitionContext)
+
+        UIView.animate(
+            withDuration: transitionDuration(using: transitionContext),
+            delay: 0,
+            options: .curveLinear,
+            animations: {
+                from.view.transform = CGAffineTransform(translationX: 0, y: from.view.frame.height)
+        }, completion: { [weak self] _ in
+            from.view.transform = .identity
+            self?.parent.onFinishTransition?(transitionContext)
+            transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+            }
+        )
+    }
+
+    func animationEnded(_ transitionCompleted: Bool) {
+    }
+}
