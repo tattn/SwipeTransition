d1b51093f09c3b09f5fc95b3587d832b5d23816b
Andrew Breckenridge <asbreckenridge@me.com>
2020-09-29 23:18:15 -0700

    Create new module: DelegateProxy, move .h files to include dir

--- Package.swift
+++ Package.swift
@@ -1,17 +1,29 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 
 import PackageDescription
 
 let package = Package(
-  name: "SwipeTransition",
-  products: [
-    .library(
-      name: "SwipeTransition",
-      targets: ["SwipeTransition"])
-  ],
-  targets: [
-    .target(
-      name: "SwipeTransition",
-      path: "Sources/SwipeTransition")
-  ]
+    name: "SwipeTransition",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "SwipeTransition",
+            targets: ["SwipeTransition"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "SwipeTransition",
+            dependencies: ["DelegateProxy"],
+            path: "Sources/SwipeTransition"
+        ),
+        .target(
+            name: "Automation",
+            path: "Sources/Automation"
+        ),
+        .target(
+            name: "DelegateProxy",
+            path: "Sources/DelegateProxy"
+        )
+    ]
 )
